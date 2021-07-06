b5609c9fa48162a302d522c6a0937b6c3cc3ec61
MyNameIsToan <18110055@student.hcmute.edu.vn>
2021-07-06 17:28:33 +0700

    aviationwebsite

--- src/main/java/com/aviationwebsite/SERVICE/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.aviationwebsite.SERVICE;
-
-public interface IUserService {
-	int CheckLogin(String username, String password);
-	int CheckValid(String username);
-	int Register (String username, String password, String email);
-}
